1dcfc09e2f8e1c38b381d95dbe38210eb13564c1
kazhurka <42044750+kazhurka@users.noreply.github.com>
2020-10-02 14:53:18 +0300

    Добавляет маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Main from "../main/main";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Favorites from "../favorites/favorites";
+import Room from "../room/room";
+import SignIn from "../sign-in/sign-in";
 
 const App = (props) => {
   const {offersNumber} = props;
-  return <Main offersNumber={offersNumber} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main offersNumber={offersNumber} />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id">
+          <Room />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
